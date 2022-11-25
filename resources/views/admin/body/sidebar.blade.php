54ecb5222f74053e5d744ed664da30da2389190b
ayoubbr <ayoub1benmansour@gmail.com>
2022-11-25 11:45:37 +0100

    add active class to sidebar items

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,10 +1,14 @@
 <aside class="main-sidebar">
+    @php
+        $prefix = Request::route()->getPrefix();
+        $route = Route::current()->getName();
+    @endphp
     <!-- sidebar-->
     <section class="sidebar">
 
         <div class="user-profile">
             <div class="ulogo">
-                <a href="{{route('dashboard')}}">
+                <a href="{{ route('dashboard') }}">
                     <!-- logo for regular state and mobile devices -->
                     <div class="d-flex align-items-center justify-content-center">
                         <img src="{{ asset('backend/images/logo-dark.png') }}" alt="">
@@ -17,14 +21,14 @@
         <!-- sidebar menu-->
         <ul class="sidebar-menu" data-widget="tree">
 
-            <li>
-                <a href="{{route('dashboard')}}">
+            <li class=" {{ $route == 'dashboard' ? 'active' : '' }}">
+                <a href="{{ route('dashboard') }}">
                     <i data-feather="pie-chart"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
 
-            <li class="treeview">
+            <li class="treeview {{ $prefix == '/users' ? 'active' : '' }}">
                 <a href="#">
                     <i data-feather="message-circle"></i>
                     <span>Manage User</span>
@@ -33,12 +37,13 @@
                     </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="{{route('users.view')}}"><i class="ti-more"></i>View User</a></li>
-                    <li><a href="{{route('users.add')}}"><i class="ti-more"></i>Add User</a></li>
+                    <li><a href="{{ route('users.view') }}"><i class="ti-more"></i>View User</a></li>
+                    <li><a href="{{ route('users.add') }}"><i class="ti-more"></i>Add User</a></li>
                 </ul>
             </li>
-            {{-- {{ $prefix == '/profile' ? 'active' : '' }} --}}
-            <li class="treeview">
+
+            {{--  --}}
+            <li class="treeview {{ $prefix == '/profile' ? 'active' : '' }}">
                 <a href="#">
                     <i data-feather="grid"></i> <span>Manage Profile</span>
                     <span class="pull-right-container">
@@ -178,13 +183,13 @@
 
     <div class="sidebar-footer">
         <!-- item-->
-        <a href="javascript:void(0)" class="link" data-toggle="tooltip" title=""
-            data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
+        <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings"
+            aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
         <!-- item-->
-        <a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title=""
-            data-original-title="Email"><i class="ti-email"></i></a>
+        <a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i
+                class="ti-email"></i></a>
         <!-- item-->
-        <a href="javascript:void(0)" class="link" data-toggle="tooltip" title=""
-            data-original-title="Logout"><i class="ti-lock"></i></a>
+        <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i
+                class="ti-lock"></i></a>
     </div>
 </aside>
