d1b89cd3e676a9e460336630ee6e4dc96ccf2aca
ayoubbr <ayoub1benmansour@gmail.com>
2023-01-19 10:09:37 +0100

    employe rege edit and details added

--- app/Http/Controllers/Backend/Employee/EmployeeRegController.php
+++ app/Http/Controllers/Backend/Employee/EmployeeRegController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\EmployeeSallaryLog;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use niklasravnsborg\LaravelPdf\Facades\Pdf;
 
 class EmployeeRegController extends Controller
 {
@@ -104,39 +105,41 @@ class EmployeeRegController extends Controller
     }
 
 
-    // public function EmployeeUpdate(Request $request, $id){
-    // 	$user = User::find($id);
-    // 	$user->name = $request->name;
-    // 	$user->fname = $request->fname;
-    // 	$user->mname = $request->mname;
-    // 	$user->mobile = $request->mobile;
-    // 	$user->address = $request->address;
-    // 	$user->gender = $request->gender;
-    // 	$user->religion = $request->religion;
+    public function EmployeeUpdate(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->fname = $request->fname;
+        $user->mname = $request->mname;
+        $user->mobile = $request->mobile;
+        $user->address = $request->address;
+        $user->gender = $request->gender;
+        $user->religion = $request->religion;
 
-    // 	$user->designation_id = $request->designation_id;
-    // 	$user->dob = date('Y-m-d',strtotime($request->dob));
+        $user->designation_id = $request->designation_id;
+        $user->dob = date('Y-m-d', strtotime($request->dob));
 
-    // 	if ($request->file('image')) {
-    // 		$file = $request->file('image');
-    // 		@unlink(public_path('upload/employee_images/'.$user->image));
-    // 		$filename = date('YmdHi').$file->getClientOriginalName();
-    // 		$file->move(public_path('upload/employee_images'),$filename);
-    // 		$user['image'] = $filename;
-    // 	}
-    //     $user->save();
-    // 	$notification = array(
-    // 		'message' => 'Employee Registration Updated Successfully',
-    // 		'alert-type' => 'success'
-    // 	);
-    // 	return redirect()->route('employee.registration.view')->with($notification);
-    // }// END METHOD
+        if ($request->file('image')) {
+            $file = $request->file('image');
+            @unlink(public_path('upload/employee_images/' . $user->image));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/employee_images'), $filename);
+            $user['image'] = $filename;
+        }
+        $user->save();
+        $notification = array(
+            'message' => 'Employee Registration Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('employee.registration.view')->with($notification);
+    }
 
 
-    // public function EmployeeDetails($id){
-    // 	$data['details'] = User::find($id);
-    // $pdf = PDF::loadView('backend.employee.employee_reg.employee_details_pdf', $data);
-    // $pdf->SetProtection(['copy', 'print'], '', 'pass');
-    // return $pdf->stream('document.pdf');
-    // }
+    public function EmployeeDetails($id)
+    {
+        $data['details'] = User::find($id);
+        $pdf = PDF::loadView('backend.employee.employee_reg.employee_details_pdf', $data);
+        $pdf->SetProtection(['copy', 'print'], '', 'pass');
+        return $pdf->stream('document.pdf');
+    }
 }
